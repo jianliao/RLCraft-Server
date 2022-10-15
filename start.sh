f1de67e45a1a9cc732c2953297033410d2af0687
jianliao <jian.liao@gmail.com>
2022-10-14 17:42:07 -0700

    Upgrade to latest RLCraft

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 MINECRAFT_VER=1.12.2
-FORGE_VER=14.23.5.2855
+FORGE_VER=14.23.5.2860
 
 echo "Downloading Forge..."
 FORGE_URL=https://files.minecraftforge.net/maven/net/minecraftforge/forge/${MINECRAFT_VER}-${FORGE_VER}/forge-${MINECRAFT_VER}-${FORGE_VER}-installer.jar
 wget "$FORGE_URL" -O installer.jar
 
 echo "Downloading RLCraft..."
-RLCRAFT_URL=$(curl https://addons-ecs.forgesvc.net/api/v2/addon/nothing/file/2935323/download-url)
-wget "$RLCRAFT_URL" -O rlcraft.zip
+#RLCRAFT_URL=$(curl https://mediafilez.forgecdn.net/files/3655/676/RLCraft+Server+Pack+1.12.2+-+Release+v2.9.1c.zip)
+#wget "$RLCRAFT_URL" -O rlcraft.zip
+wget https://mediafilez.forgecdn.net/files/3655/676/RLCraft+Server+Pack+1.12.2+-+Release+v2.9.1c.zip -O rlcraft.zip
 unzip -q rlcraft.zip
 rm -rf rlcraft.zip
