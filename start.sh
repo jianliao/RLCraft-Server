e0c282cac6fffdd2117c779e37f58ae194442738
jianliao <jian.liao@gmail.com>
2022-10-19 22:39:22 -0700

    Switch to RLCraft 2.8.2

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 MINECRAFT_VER=1.12.2
-FORGE_VER=14.23.5.2860
+FORGE_VER=14.23.5.2838
 
 echo "Downloading Forge..."
 FORGE_URL=https://files.minecraftforge.net/maven/net/minecraftforge/forge/${MINECRAFT_VER}-${FORGE_VER}/forge-${MINECRAFT_VER}-${FORGE_VER}-installer.jar
 wget "$FORGE_URL" -O installer.jar
 
 echo "Downloading RLCraft..."
-RLCRAFT_URL=https://mediafilez.forgecdn.net/files/3655/676/RLCraft+Server+Pack+1.12.2+-+Release+v2.9.1c.zip
+#RLCRAFT_URL=https://mediafilez.forgecdn.net/files/3655/676/RLCraft+Server+Pack+1.12.2+-+Release+v2.9.1c.zip
+RLCRAFT_URL=https://mediafilez.forgecdn.net/files/2935/323/RLCraft+Server+Pack+1.12.2+-+Beta+v2.8.2.zip
 wget "$RLCRAFT_URL" -O rlcraft.zip
 
 echo "Unzip RLCraft..."
